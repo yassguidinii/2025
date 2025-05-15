fb60c6ffd8b13fc1d8fef9de5f434fe06a4cc1ee
yassguidinii <03235557059@senacrs.edu.br>
2025-05-15 12:18:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h1> Primeiro Programa PHP </h1>";
+        echo "<br>";
+        echo "Olá Mundo";
+        $nome =  "Yasmin";
+        echo "<br>";
+        echo "Bom dia $nome!";
+        ?>
+    </body>
+</html>
